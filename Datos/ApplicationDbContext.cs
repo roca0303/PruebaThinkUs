f6b7e90e890ddd0bc1742595c8e11571e17267b7
rocaa0303 <ro_campeon@hotmail.com>
2024-10-02 12:37:01 -0500

    Agregar archivos de proyecto.

---
+++ Datos/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaThinkUs.Models;
+
+namespace PruebaThinkUs.Datos
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Agregar los modelos aquí (Cada modelo corresponde a una tabla en la BD)
+        public DbSet<Empleado> Empleado { get; set; }
+
+    }
+}
---
+++ Models/Empleado.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaThinkUs.Models
+{
+    public class Empleado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        public string? Telefono { get; set; }
+
+        [Required(ErrorMessage = "El celular es obligatorio")]
+        public string? Celular { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public string? Email { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
